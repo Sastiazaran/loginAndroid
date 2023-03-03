2e52408bdd1145cbf12d15e8ea5fb343432f42e0
unknown <CIAC@CIAC2-007.academic.mixcoac.upmx.mx>
2023-03-03 12:03:50 -0600

    production commit 4

--- app/src/main/java/com/example/practica3_login/Principal.kt
+++ app/src/main/java/com/example/practica3_login/Principal.kt
@@ -3,7 +3,11 @@ package com.example.practica3_login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -18,6 +22,9 @@ class Principal : AppCompatActivity() {
 
         auth = Firebase.auth
 
+        val toolbar = findViewById<Toolbar>(R.id.toolbar3)
+        setSupportActionBar(toolbar)
+
         var logout = findViewById<Button>(R.id.logout)
 
         logout.setOnClickListener(){
@@ -26,5 +33,24 @@ class Principal : AppCompatActivity() {
             finish()
         }
 
+    }
+
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.salir)
+        {
+            Toast.makeText(this, "opcion salir", Toast.LENGTH_LONG).show()
+        }
+        else if(item.itemId == R.id.perfil)
+        {
+            Toast.makeText(this, "opcion perfil", Toast.LENGTH_LONG).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
